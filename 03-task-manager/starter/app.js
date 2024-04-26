5f93bd8e141c7a99bacb4619ed7043401e04c112
AnujaBujurge29 <anuja.bujurge29@gmail.com>
2024-04-26 16:31:36 -0500

    Week 5 Assessment

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,42 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const port = 3000
+
+//Middleware
+app.use(express.json())
+
+// Routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+// Get all the tasks
+// app.get('/api/v1/tasks')
+
+// Create a new Task
+// app.post('/api/v1/tasks')
+
+// Get Single task
+// app.get('/api/v1/tasks/:id')
+
+// Update Task
+// app.patch('/api/v1/tasks/:id')
+
+//delete Taks
+// app.delete('/api/v1/tasks/:id')
+
+const start = async () => {
+    try {
+        await connectDB(process.env.Mongo_URL)
+        app.listen(port, console.log(`Server is listening at port ${port}`))
+    }
+    catch (err) { console.log(err); }
+}
+
+start()
---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const connectDB = (MongoURL) => {
+    return mongoose.connect(MongoURL)
+}
+
+module.exports = connectDB
